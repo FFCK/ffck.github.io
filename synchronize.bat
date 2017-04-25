5e7f4742818803176ef7b188b499fa6659383bcb
Yoyock <lifraisse@gmail.com>
2017-04-25 11:06:19 +0200

    copy photos

--- synchronize.bat
+++ synchronize.bat
@@ -1,5 +1,6 @@
 cd "C:\Users\benoit\Documents\GitHub\ffck.github.io"
 xcopy /s /d /y "C:\Users\benoit\Desktop\Gestion des Courses 2017\National\France Elite 2017\Base 4D - France Elite 2017\progresult" "C:\Users\benoit\Documents\GitHub\ffck.github.io\franceelite"
+xcopy /s /d /y "\\Doom\competition\PHOTOWEB" "C:\Users\benoit\Documents\GitHub\ffck.github.io\franceelite"
 git commit -m "mise a jour resultats courses"
 git pull --rebase origin master
 git push origin master
