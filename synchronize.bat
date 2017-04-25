2a67f90587b8206a68cf5bac0884fb94747f56bb
Yoyock <lifraisse@gmail.com>
2017-04-25 10:18:10 +0200

    Synchronisation en local

--- synchronize.bat
+++ synchronize.bat
@@ -1,4 +1,4 @@
-xcopy /s /d /y C:\source C:\destination
+xcopy /s /d /y "C:\Users\benoit\Desktop\Gestion des Courses 2017\National\France Elite 2017\Base 4D - France Elite 2017\progresult" "C:\Users\benoit\Documents\GitHub\ffck.github.io\franceelite"
 git commit -m "mise a jour resultats courses"
 git pull --rebase origin master
 git push origin master
