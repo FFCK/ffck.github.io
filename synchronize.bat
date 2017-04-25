55f030a4d457270e2437306e4ee653e0f540fae1
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2017-04-25 10:26:46 +0200

    Ajout d'une commande

--- synchronize.bat
+++ synchronize.bat
@@ -1,3 +1,4 @@
+cd "C:\Users\benoit\Documents\GitHub\ffck.github.io"
 xcopy /s /d /y "C:\Users\benoit\Desktop\Gestion des Courses 2017\National\France Elite 2017\Base 4D - France Elite 2017\progresult" "C:\Users\benoit\Documents\GitHub\ffck.github.io\franceelite"
 git commit -m "mise a jour resultats courses"
 git pull --rebase origin master
